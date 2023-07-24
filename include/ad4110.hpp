083a4e1f8ad6735d34bffc69170ec68219c0008f
melektron <matteo@elektron.work>
2023-07-24 11:27:58 +0200

    implemented register read/write (not tested)

--- include/ad4110.hpp
+++ include/ad4110.hpp
@@ -13,6 +13,9 @@
 
 #include <stdint.h>
 #include <uc_gpio.hpp>
+#include <el/retcode.hpp>
+#include <FreeRTOS.h>
+#include <semphr.h>
 
 
 #define AD_READ         0b01000000
@@ -57,24 +60,138 @@
 #define AD_ADC_REG_ADC_GAIN3            0x0f
 
 
+// general defines
+// SPI R/W buffer size [bytes]
+#define AD_SPI_BUFFER_SIZE 8
+
+// register size enums used for general register reading and writing.
+// this enumerates to the size int bytes.
+enum ad_reg_size_t
+{
+    AD_REG_8 = 1,
+    AD_REG_16 = 2,
+    AD_REG_24 = 3,
+};
+
 class AD4110
 {
-private:
+private:    // data
+    // The SPI peripheral the AD4110-1 chip is connected to
+    SPI_HandleTypeDef &hspi;
     // The address of the AD4110-1 chip on the bus (this chip has a special way of using addresses over SPI)
     uint8_t address;
     // The CS pin the AD4110-1 is connected to (can be shared with multiple AD4110-1 if they have different pins)
-    uc::Pin cs_pin;
+    uc::Pin &cs_pin;
+
+    // SPI communication buffers
+    uint8_t spi_write_buffer[AD_SPI_BUFFER_SIZE];
+    uint8_t spi_read_buffer[AD_SPI_BUFFER_SIZE];
+
+    // semaphore to guard SPI peripheral
+    StaticSemaphore_t sem_buffer_spi_guard;
+    SemaphoreHandle_t sem_spi_guard = nullptr;
+
+    // global timeout for any operation
+    TickType_t timeout_ticks;
+
+private:    // methods
+
+    /**
+     * @brief transmits _size bytes from the SPI write buffer. This function handles
+     * the access semaphore to avoid race conditions. If the semaphore is taken,
+     * this function will block for the selected timeout value and return nolock on timeout.
+     * 
+     * @param _size number of bytes to be transmitted
+     * 
+     * @return el::retcode 
+     * @retval ok - write successful
+     * @retval busy - SPI already busy
+     * @retval err - some other SPI error
+     * @retval nolock - SPI guard semaphore timeout
+     */
+    el::retcode transmitBytes(uint8_t _size);
+
+    /**
+     * @brief transmits _size bytes from the SPI write buffer and receives the same
+     * amount of bytes in the SPI read buffer. This function handles
+     * the access semaphore to avoid race conditions. If the semaphore is taken,
+     * this function will block for the selected timeout value and return nolock on timeout.
+     * 
+     * @param _size number of bytes to be xmitted
+     * 
+     * @return el::retcode 
+     * @retval ok - read/write successful
+     * @retval busy - SPI already busy
+     * @retval err - some other SPI error
+     * @retval nolock - SPI guard semaphore timeout
+     */
+    el::retcode xmitBytes(uint8_t _size);
+
+
 
 public:
 
     /**
      * @brief Construct a new AD4110 driver instance
      *
+     * @param _hspi The SPI peripheral the AD4110-1 chip is connected to
      * @param _addr The address of the AD4110-1 chip on the bus (this chip has a special way of using addresses over SPI)
      * @param _cs_pin The CS pin the AD4110-1 is connected to (can be shared with multiple AD4110-1 if they have different pins)
+     * @param _timeout The timeout used for all operations needing to take peripheral semaphores
      */
     AD4110(
+        SPI_HandleTypeDef &_hspi,
         uint8_t _addr,
-        uc::Pin _cs_pin
+        uc::Pin &_cs_pin,
+        TickType_t _timeout = 1000
     );
+
+    /**
+     * @brief resets the AD4110-1 chip by writing 64 1s to it.
+     * 
+     * @return el::retcode 
+     * @retval ok - write successful
+     * @retval busy - SPI already busy
+     * @retval err - some other SPI error or invalid parameter
+     * @retval nolock - SPI guard semaphore timeout
+     */
+    el::retcode reset();
+
+    /**
+     * @brief writes to a register of the AD4110-1. It simply sends the write command and then
+     * the data.
+     * 
+     * @param _reg register definition
+     * @param _size size of data to be written. use enumerations.
+     * @param _data data to be written. Only the amount of bytes defined by the data size is
+     * written, right aligned (size 8 writes the lowest byte, 16 the two lowest bytes and 24
+     * the three lowest bytes. The highest byte is always ignored.)
+     * 
+     * @return el::retcode 
+     * @retval ok - write successful
+     * @retval busy - SPI already busy
+     * @retval err - some other SPI error or invalid parameter
+     * @retval nolock - SPI guard semaphore timeout
+     * @retval invalid - invalid size
+     */
+    el::retcode writeRegister(uint8_t _reg, ad_reg_size_t _size, uint32_t _data);
+
+    /**
+     * @brief reads to a register from the AD4110-1. It simply sends the read command and then
+     * reads
+     * 
+     * @param _reg register definition
+     * @param _size size of data to be written. use enumerations.
+     * @param _data [out] data to be written. Only the amount of bytes defined by the data size is
+     * written, right aligned (size 8 writes the lowest byte, 16 the two lowest bytes and 24
+     * the three lowest bytes. The highest byte is always ignored.)
+     * 
+     * @return el::retcode 
+     * @retval ok - write successful
+     * @retval busy - SPI already busy
+     * @retval err - some other SPI error or invalid parameter
+     * @retval nolock - SPI guard semaphore timeout
+     * @retval invalid - invalid size
+     */
+    el::retcode readRegister(uint8_t _reg, ad_reg_size_t _size, uint32_t *_data);
 };
--- src/ad4110.cpp
+++ src/ad4110.cpp
@@ -9,14 +9,162 @@
  *
  */
 
+#include <stdint.h>
+#include <string.h>
+#include <assert.h>
+
 #include "ad4110.hpp"
 
 AD4110::AD4110(
+    SPI_HandleTypeDef &_hspi,
     uint8_t _addr,
-    uc::Pin _cs_pin
+    uc::Pin &_cs_pin,
+    TickType_t _timeout
 ) :
+    hspi(_hspi),
     address(_addr),
-    cs_pin(_cs_pin)
+    cs_pin(_cs_pin),
+    timeout_ticks(_timeout)
 {
-    
+    sem_spi_guard = xSemaphoreCreateBinaryStatic(&sem_buffer_spi_guard);
+    configASSERT(sem_spi_guard);
+}
+
+el::retcode AD4110::transmitBytes(uint8_t _size)
+{
+    el::retcode retval = el::retcode::ok;
+    HAL_StatusTypeDef status;
+
+    if (!xSemaphoreTake(sem_spi_guard, timeout_ticks))
+        return el::retcode::nolock;
+
+    // just to make sure, should not happen with semaphore
+    if (hspi.State != HAL_SPI_STATE_READY)
+    {
+        retval = el::retcode::busy;
+        goto cleanup;
+    }
+
+    // start write
+    cs_pin.write(0);
+    status = HAL_SPI_Transmit(&hspi, spi_write_buffer, _size, timeout_ticks);
+
+    // check outcome (in case busy flag changed while preparing data or another error occurred)
+    if (status == HAL_BUSY)
+    {
+        // don't reset pin because it is probably used by 
+        // something else (should not happen normally anyway)
+        retval = el::retcode::busy;
+        goto cleanup;
+    }
+    else if (status != HAL_OK)
+    {
+        cs_pin.write(1);
+        retval = el::retcode::err;
+        goto cleanup;
+    }
+
+cleanup:
+    xSemaphoreGive(sem_spi_guard);
+    return retval;
+}
+
+el::retcode AD4110::xmitBytes(uint8_t _size)
+{
+    el::retcode retval = el::retcode::ok;
+    HAL_StatusTypeDef status;
+
+    if (!xSemaphoreTake(sem_spi_guard, timeout_ticks))
+        return el::retcode::nolock;
+
+    // just to make sure, should not happen with semaphore
+    if (hspi.State != HAL_SPI_STATE_READY)
+    {
+        retval = el::retcode::busy;
+        goto cleanup;
+    }
+
+    // start transfer
+    cs_pin.write(0);
+    status = HAL_SPI_TransmitReceive(&hspi, spi_write_buffer, spi_read_buffer, _size, timeout_ticks);
+
+    // check outcome (in case busy flag changed while preparing data or another error occurred)
+    if (status == HAL_BUSY)
+    {
+        // don't reset pin because it is probably used by 
+        // something else (should not happen normally anyway)
+        retval = el::retcode::busy;
+        goto cleanup;
+    }
+    else if (status != HAL_OK)
+    {
+        cs_pin.write(1);
+        retval = el::retcode::err;
+        goto cleanup;
+    }
+
+cleanup:
+    xSemaphoreGive(sem_spi_guard);
+    return retval;
+}
+
+el::retcode AD4110::writeRegister(uint8_t _reg, ad_reg_size_t _size, uint32_t _data)
+{
+    // prepare data
+    uint8_t data_len = _size + 1;   // +1 byte for command
+    spi_write_buffer[0] = AD_WRITE | AD_ADDR(address) | _reg;
+    if (_size == AD_REG_8)
+    {
+        spi_write_buffer[1] = (uint8_t)(_data & 0xff);
+    }
+    else if (_size == AD_REG_16)
+    {
+        spi_write_buffer[1] = (uint8_t)(_data >> 1 & 0xff);
+        spi_write_buffer[2] = (uint8_t)(_data & 0xff);
+    }
+    else if (_size == AD_REG_24)
+    {
+        spi_write_buffer[1] = (uint8_t)(_data >> 2 & 0xff);
+        spi_write_buffer[2] = (uint8_t)(_data >> 1 & 0xff);
+        spi_write_buffer[3] = (uint8_t)(_data & 0xff);
+    }
+    else
+        return el::retcode::invalid;
+
+    return transmitBytes(data_len);
+}
+
+el::retcode AD4110::readRegister(uint8_t _reg, ad_reg_size_t _size, uint32_t *_data)
+{
+    // prepare command
+    uint8_t data_len = _size + 1;   // +1 byte for command
+    spi_write_buffer[0] = AD_READ | AD_ADDR(address) | _reg;
+
+    el::retcode retval = xmitBytes(data_len);
+
+    // process the data
+    if (_size == AD_REG_8)
+    {
+        *_data = spi_read_buffer[1];
+    }
+    else if (_size == AD_REG_16)
+    {
+        *_data = spi_read_buffer[1] << 8 | spi_read_buffer[2];
+    }
+    else if (_size == AD_REG_24)
+    {
+        *_data = spi_read_buffer[1] << 16 | spi_read_buffer[2] << 8 | spi_read_buffer[3];
+    }
+    else
+        return el::retcode::invalid;
+
+    return retval;
+}
+
+el::retcode AD4110::reset()
+{
+    uint8_t data_len = 8;
+    memset(spi_write_buffer, 0xff, data_len);
+
+    return transmitBytes(data_len);
 }
